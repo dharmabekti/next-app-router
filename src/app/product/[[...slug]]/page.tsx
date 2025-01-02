035ce667a93965cc705858a701bee4ae3c70d389
dharmabekti <dharmabekti@gmail.com>
2025-01-02 15:37:23 +0700

    Data Fetching

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,21 +1,33 @@
 import { use } from "react";
 
-type DetailProductPageProps = {
+type ProductPageProps = {
   params: Promise<{
     slug: string[];
   }>;
 };
 
-export default function DetailProductPage({ params }: DetailProductPageProps) {
+async function getData() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return res.json();
+}
+
+export default function ProductPage({ params }: ProductPageProps) {
   const unwrappedParams = use(params); // Menggunakan React.use untuk unwrap
   const slug = unwrappedParams.slug;
+  // const data = await getData();
+  // console.log(data);
 
   return (
     <div>
-      <h1>Detail Product Page</h1>
-      <p>Category: {slug[0]}</p>
-      <p>Gender: {slug[1]}</p>
-      <p>Id: {slug[2]}</p>
+      <h1>{slug ? "Detail Product Page" : "Product Page"}</h1>
+      {slug && (
+        <>
+          <p>Category: {slug[0]}</p>
+          <p>Gender: {slug[1]}</p>
+          <p>Id: {slug[2]}</p>
+        </>
+      )}
     </div>
   );
 }
