d85f896a68b49e5da12b3feda7f18b2aa76c5787
dharmabekti <dharmabekti@gmail.com>
2025-04-13 22:04:30 +0700

    Intercepting Route

---
+++ src/services/products/index.tsx
@@ -0,0 +1,12 @@
+export const getData = async (url: string) => {
+  const res = await fetch(url);
+  // const res = await fetch("http://localhost:3000/api/product", {
+  //   cache: "no-cache",
+  //   next: {
+  //     tags: ["products"],
+  //     // revalidate: 20
+  //   },
+  // });
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return res.json();
+};
